a661097a980c1f05ac604fd91138ec679bae624a
Harsh_G <hgaur491@gmail.com>
2026-08-22 16:05:54 +0530

    setting up drizzle + node:sqlite

---
+++ src/env.ts
@@ -0,0 +1,3 @@
+export const env = {
+    DB_FILE_NAME: process.env.DB_FILE_NAME ?? "./src/lib/db/local.db",
+}
---
+++ src/lib/db/index.ts
@@ -0,0 +1,8 @@
+import { env } from "#/env"
+import "dotenv/config"
+import { drizzle } from "drizzle-orm/node-sqlite"
+import { DatabaseSync } from "node:sqlite"
+import { relations } from "./relations"
+
+const sqlite = new DatabaseSync(env.DB_FILE_NAME)
+export const db = drizzle({ client: sqlite, relations })
